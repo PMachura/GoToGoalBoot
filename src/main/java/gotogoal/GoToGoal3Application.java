2b86a0c6d5ffaf565412c2f463403cd5ad635ff1
PMachura <przemekmachura94@gmail.com>
2016-08-19 05:49:36 +0200

    Project initialization

---
+++ src/main/java/gotogoal/GoToGoal3Application.java
@@ -0,0 +1,12 @@
+package gotogoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoToGoal3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoToGoal3Application.class, args);
+	}
+}

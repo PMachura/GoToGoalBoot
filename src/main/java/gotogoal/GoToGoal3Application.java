d2b2b5cc2e9408cdb5899870eecfc47ef26edc94
PMachura <przemekmachura94@gmail.com>
2016-08-26 09:14:26 +0200

    Picture uploading

--- src/main/java/gotogoal/GoToGoal3Application.java
+++ src/main/java/gotogoal/GoToGoal3Application.java
@@ -2,8 +2,13 @@ package gotogoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+import gotogoal.config.PictureUploadProperties;
+
+@SpringBootApplication(exclude={HibernateJpaAutoConfiguration.class})
+@EnableConfigurationProperties({PictureUploadProperties.class})
 public class GoToGoal3Application {
 
 	public static void main(String[] args) {
